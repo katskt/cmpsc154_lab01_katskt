307d876d27965126ee7e27e5f186b3b7a16df8a6
Soome Pyo <soome@ucsb.edu>
2026-01-13 15:27:18 -0800

    clock

--- ucsbcs154lab1_clock.py
+++ ucsbcs154lab1_clock.py
@@ -14,32 +14,49 @@ How many bits do you need to represent the time? (You want to use the minimum am
 How do you update registers?
 """
 # Initialize outputs
-output_seconds = pyrtl.Output(...) # name="output_seconds"
-output_minutes = pyrtl.Output(...) # name="output_minutes"
-output_hours = pyrtl.Output(...)   # name="output_hours"
+output_seconds = pyrtl.Output(bitwidth = 6, name = "output_seconds") # name="output_seconds", 2^6 = 64
+output_minutes = pyrtl.Output(bitwidth = 6, name = "output_minutes") # name="output_minutes"
+output_hours = pyrtl.Output(bitwidth = 5, name = "output_hours")   # name="output_hours", 2^ 5 = 32
 
 # Initialize Registers Here
-seconds = pyrtl.Register(...)
-minutes = pyrtl.Register(...)
-hours = pyrtl.Register(...)
+seconds = pyrtl.Register(bitwidth = 6, name = "seconds") 
+minutes = pyrtl.Register(bitwidth = 6, name = "minutes") 
+hours = pyrtl.Register(bitwidth = 5, name = "hours") 
 
 # Put Sequential Logic Here
 # update the seconds register here
 with pyrtl.conditional_assignment:
-    ...
-
+    with seconds != 59:
+        temp1 = pyrtl.Const(1, bitwidth = 6)
+        seconds.next |= seconds + temp1
+    with pyrtl.otherwise:
+        seconds.next |= pyrtl.Const(0, bitwidth = 6)
 
 # update the minutes register here
 with pyrtl.conditional_assignment:
-    ...
-
+    with seconds == 59:
+        with minutes != 59:
+            minutes.next |= minutes + temp1
+    with seconds == 59:
+        with minutes == 59:
+            minutes.next |= pyrtl.Const(0, bitwidth = 6)
 # update the hours register here
 with pyrtl.conditional_assignment:
-    ...
+    with seconds == 59:
+        with minutes == 59:
+            with hours != 23:
+                temp2 = pyrtl.Const(1, bitwidth = 5)
+                hours.next |= hours + temp2
+    with seconds == 59:
+        with minutes == 59:
+            with hours == 23:
+                hours.next |= pyrtl.Const(0, bitwidth = 5)
 
 
 # Assign your outputs here
-
+output_seconds <<= seconds
+output_minutes <<= minutes
+output_hours <<= hours
 
 
 # Simulation Code
@@ -51,10 +68,10 @@ if __name__ == "__main__":
         sim.step({})
 
     ### Uncomment the following line to the whole trace
-    # sim_trace.render_trace(repr_func=str)
+    sim_trace.render_trace(repr_func=str)
 
     ### Uncomment the following lines to see the most recent values of hours, minutes, and seconds
-    # print("hours:", sim_trace.trace["output_hours"][-1])
-    # print("minutes:", sim_trace.trace["output_minutes"][-1])
-    # print("seconds:", sim_trace.trace["output_seconds"][-1])
+    print("hours:", sim_trace.trace["output_hours"][-1])
+    print("minutes:", sim_trace.trace["output_minutes"][-1])
+    print("seconds:", sim_trace.trace["output_seconds"][-1])
 
